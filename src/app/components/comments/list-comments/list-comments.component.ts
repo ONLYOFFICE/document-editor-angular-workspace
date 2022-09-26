4c031b7817542cab6dd3bf8318f21d97c76507f3
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2022-09-26 20:43:13 +0300

    events onAddComment, onRemoveComment, onChangeCommentData

--- src/app/components/comments/list-comments/list-comments.component.ts
+++ src/app/components/comments/list-comments/list-comments.component.ts
@@ -1,18 +1,47 @@
-import {Component, Input, SimpleChanges} from '@angular/core'
+import {Component, Input, SimpleChanges, OnChanges} from '@angular/core'
 
 @Component({
   selector: 'list-comments',
   templateUrl: './list-comments.component.html',
   styleUrls: ['../comments.component.css']
 })
-export class ListCommentsComponent {
-  @Input() connector: any[];
+export class ListCommentsComponent implements OnChanges {
+  @Input() connector: any;
   @Input() userName: string;
   @Input() comments: any[];
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log("sdfsadf");
-    console.log(changes);
+    if (changes.hasOwnProperty("connector")) {
+      if (this.connector) {
+        this.connector.attachEvent("onAddComment",  function (val:any) {
+          console.log("onAddComment");
+          var comments = this.comments;
+          var index = this.comments.findIndex((comment:any) => comment.Id === val.Id)
+
+          if (index == -1) {
+             this.comments = [val, ...comments];
+          }
+        }.bind(this));
+
+        this.connector.attachEvent("onRemoveComment",  function (val:any) {
+          console.log("onRemoveComment");
+          const index = this.comments.findIndex((comment:any) => comment.Id === val.Id);
+
+          if (index !== -1) {
+            this.comments.splice(index, 1);
+          }
+        }.bind(this));
+
+        this.connector.attachEvent("onChangeCommentData",  function (val:any) {
+          const index = this.comments.findIndex((comment:any) => comment.Id === val.Id);
+          console.log(index);
+
+          if (index !== -1) {
+            this.comments[index].Data = val.Data;
+          }
+        }.bind(this));
+      }
+    }
   }
 
   dateParser(date: string) {
