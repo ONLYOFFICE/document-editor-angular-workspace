fa6ad34733036d93d560a17dea2d2d74463d66b2
Sergey Linnik <sergey.linnik@onlyoffice.com>
2024-02-27 10:46:14 +0300

    change oform to pdf

--- src/app/components/forms/forms-stories/forms-stories.component.ts
+++ src/app/components/forms/forms-stories/forms-stories.component.ts
@@ -29,7 +29,7 @@ export class FormsStoriesComponent implements OnInit {
 
     newConfig.document.title = this.selectedForm.title;
     newConfig.document.url = this.selectedForm.url;
-    newConfig.document.key = "oform" + Math.random();
+    newConfig.document.key = "pdf" + Math.random();
 
     this.config = newConfig;
   }
