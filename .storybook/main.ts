665f40f8b78e6a5e9db184ae3614b22bede86d9d
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2026-03-04 14:46:24 +0300

    chore: update Storybook config and dependencies for Angular 21

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,11 @@
 import type { StorybookConfig } from "@storybook/angular";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+
   framework: {
     name: "@storybook/angular",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  }
 };
 export default config;
